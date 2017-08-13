55dee500df142cfff1a8993d84a77a10311d4a7b
Dylan Nichols <nichodo@g.toiohomai.ac.nz>
2017-08-14 10:13:24 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9994086_task2_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
