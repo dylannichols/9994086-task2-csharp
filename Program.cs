03b756abb6b036c4564b225adfc9f93115e153aa
Dylan Nichols <nichodo@g.toiohomai.ac.nz>
2017-08-14 11:40:06 +1200

    final

--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace _9994086_task2_csharp
             //Start the program with Clear();
             Console.Clear();
             
+            // Explain program to user
             var lineBreak = "-------------------------------------------";
             Console.WriteLine("This program asks you for a set of ten number then calculates the average.");
             Console.WriteLine($"\n{lineBreak}\n");
 
+            // Get input from user, check for bad input and ask again if it is bad
             var input = "";
             var nums = new double[10];
             var isNumber = true;
@@ -30,13 +32,15 @@ namespace _9994086_task2_csharp
                 } while (!isNumber);
             }
 
+            // Calculate average
             var sum = 0.0;
             for (var i = 0; i < nums.Length; i++)
             {
                 sum += nums[i];
             }
             var average = sum / nums.Length;
-
+            
+            // Output to user
             var numberString = string.Join(", ", nums);
             Console.WriteLine($"\n{lineBreak}\n");
             Console.WriteLine($"You numbers were: {numberString}");
